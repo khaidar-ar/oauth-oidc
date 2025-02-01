21b628558484739aa4383cf0c62dd1927c0b6b1b
khaidar-ar <96arrijal@gmail.com>
2025-02-01 20:18:15 +0700

    feat : init project

---
+++ server/src/main/java/com/spring/security/oauth_oidc_server/OauthOidcServerApplication.java
@@ -0,0 +1,13 @@
+package com.spring.security.oauth_oidc_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OauthOidcServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OauthOidcServerApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/com/spring/security/oauth_oidc_server/OauthOidcServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.security.oauth_oidc_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OauthOidcServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
